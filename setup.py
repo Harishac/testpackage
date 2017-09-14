96cefce5c67193ae07e02eb923e17ef665840726
Harish C <harish.c.ac@gmail.com>
2017-09-14 23:46:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="hello",
     version="0.13.1",
-    packages=find_packages(),
+    packages=['hello'],
+    entry_points={'console_scripts': ['hello = __main__:main']},
     zip_safe=False)
