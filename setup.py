1067b2b40b57a7730ea30ed4a5e49be555e3324a
Harish C <harish.c.ac@gmail.com>
2017-09-14 23:11:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name="testhac",
+    name="hello",
     version="1.0.0",
-    packages=find_packages(),
+    packages=['hello'],
+    entry_points={'console_scripts': ['hello = __main__:main']},
     zip_safe=False)
