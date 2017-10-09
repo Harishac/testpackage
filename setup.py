403620e223cd5b999b443accfee823cc6ebd8bb9
Harish C <harish.c.ac@gmail.com>
2017-10-09 15:00:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 setup(   
-    name=os.getenv('PKG_NAME', ''),
-    version=os.getenv('PKG_VERSION', ''),
+    name=sampleapp,
+    version=0.0.1,
     packages=find_packages(exclude=['tests']),
     zip_safe=False)
